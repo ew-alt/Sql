48d61ce28b7ad5719fad4edfa8d31f3bd96edf6b
Minaam Saifuddin <79788485+ew-alt@users.noreply.github.com>
2025-04-28 00:43:26 +0500

    Push example

--- sql_course.session.sql
+++ sql_course.session.sql
@@ -1,1 +1,12 @@
-DROP DATABASE sql_course
+SELECT 
+    skills_job_dim.skill_id
+FROM skills_job_dim
+WHERE skills_job_dim.job_id IN (
+SELECT *
+FROM january_jobs
+UNION ALL
+SELECT *
+FROM febuary_jobs
+) 
+
+
